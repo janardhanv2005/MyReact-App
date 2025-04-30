0b769472fcb70ca512768de6fbd60e841b965397
janar <janardhanv2005@gmail.com>
2025-04-30 08:40:41 +0800

    Turned into basic react App

--- App.js
+++ App.js
@@ -1,44 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>My H1 from React Component</h1>
-    </div>
- </div> */
-}
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "My H1 from React Component"),React.createElement("h2", {}, "My H2 from React Component")]
-  ),React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "My H1 from React Component2"),React.createElement("h2", {}, "My H2 from React Component2")]
-  )]
-);
-
-const header = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!!"
-);
-
-const Title = () => <h1 tabindex="5">I am Title</h1>;
-const HeadingComponent = () => (
-<div id="container">
-  <Title />
-<h1> Hellow World from React Component </h1>
+const Header = () => {return (
+<div className='headerContainer'>
+<img className='res-logo' src='https://marketplace.canva.com/EAFpeiTrl4c/2/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-a1RYzvS1EFo.jpg'/>
+<ul className="nav-items">
+  <li>Home</li>
+  <li>About</li>
+  <li>Countact Us</li>
+  <li>Cart</li>
+</ul>
 </div>
-);
+)};
 
+const Footer = () => {return (
+  <div className='footerContainer'>
+  <img className='res-logo' src='https://marketplace.canva.com/EAFpeiTrl4c/2/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-a1RYzvS1EFo.jpg'/>
+  <ul className="nav-items">
+    <li>Home</li>
+    <li>About</li>
+    <li>Countact Us</li>
+    <li>Cart</li>
+  </ul>
+  </div>
+  )};
+
+const RestaurentCard = () => { 
+  return (
+  <div id='restCard' className='rest-card'>
+    <img className='rest-card-img' src='https://product-assets.faasos.io/eatsure/production/veg-biryani-online.jpeg'></img>
+    <h2>Jana's Biryani</h2>
+    <h4>South Indian, Asian</h4>
+    <h5>4.7 rating</h5>
+    <h5>29 minutes</h5>
+  </div>
+)};
+
+const Body = () => {
+  return (
+  <div>
+  <div id='body'>
+  <h2>Search</h2>
+  <div id='restCardContainer'>
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  <RestaurentCard />
+  </div>
+  </div>
+  </div>
+)};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+    <Header />
+    <Body />
+    <Footer />
+    </div>
+  )
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
-// setTimeout(()=>{
-//     root.render(parent);
-// }, 5000);
+root.render(<AppLayout />);
+
 
